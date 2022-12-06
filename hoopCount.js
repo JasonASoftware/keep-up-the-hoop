bc82c816ba7c4c101398074b149946dfc260d810
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-12-05 20:53:12 -0500

    created js file

---
+++ hoopCount.js
@@ -0,0 +1,8 @@
+function hoopCount (n) {
+//used conditional ternary operator on the hoop count
+  if(n > 9){
+    return 'Great, now move on to tricks'
+  }else{
+    return 'Keep at it until you get it'
+  }
+}
